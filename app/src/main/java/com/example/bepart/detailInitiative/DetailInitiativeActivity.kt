aa670ea5777decaf95f0f1eaf7ea28bada02c978
Victor Ignacio Paez <39324242+nacho914@users.noreply.github.com>
2022-12-23 12:27:10 -0700

    Add DetailInitiative activity functionality

---
+++ app/src/main/java/com/example/bepart/detailInitiative/DetailInitiativeActivity.kt
@@ -0,0 +1,77 @@
+package com.example.bepart.detailInitiative
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.bepart.INITIATIVE_KEY
+import com.example.bepart.R
+import com.example.bepart.databinding.ActivityDetailInitiativeBinding
+import com.example.bepart.main.model.Initiatives
+
+class DetailInitiativeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailInitiativeBinding
+    private lateinit var viewModel: DetailInitiativeViewModel
+    private lateinit var key: String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailInitiativeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        key = intent.getStringExtra(INITIATIVE_KEY).orEmpty()
+        callViewModel()
+        viewModel.addLiveListener(key, "Nacho")
+    }
+
+    private fun callViewModel() {
+        viewModel = ViewModelProvider(this)[DetailInitiativeViewModel::class.java]
+
+        val observer = setUpInitiativeObserver()
+        val statusObserver = setUpStatusObserver()
+        val votedObserver = setUpVotedObserver()
+
+        viewModel.getInitiative().observe(this, observer)
+        viewModel.getStatus().observe(this, statusObserver)
+        viewModel.getVote().observe(this, votedObserver)
+    }
+
+    private fun setUpInitiativeObserver(): Observer<Initiatives> {
+        return Observer<Initiatives> {
+            setUpData(it)
+            binding.loadingWheel.visibility = View.GONE
+        }
+    }
+
+    private fun setUpStatusObserver(): Observer<Boolean> = Observer<Boolean> {
+        when (it) {
+            true -> binding.loadingWheel.visibility = View.VISIBLE
+            false -> binding.loadingWheel.visibility = View.GONE
+        }
+    }
+
+    private fun setUpVotedObserver(): Observer<Boolean> =
+        Observer<Boolean> {
+            when (it) {
+                true -> {
+                    binding.addSupportBtn.setImageResource(R.drawable.ic_baseline_check_24)
+                    binding.addSupportBtn.setOnClickListener {
+                        viewModel.removeVote(key, "Nacho")
+                    }
+                }
+                false -> {
+                    binding.addSupportBtn.setImageResource(R.drawable.ic_add_circle_36)
+                    binding.addSupportBtn.setOnClickListener {
+                        viewModel.addVote(key, "Nacho")
+                    }
+                }
+            }
+        }
+
+    private fun setUpData(initiatives: Initiatives?) {
+        binding.nameText.text = initiatives?.nombre
+        binding.categoryText.text = initiatives?.categoria
+        binding.descriptionText.text = initiatives?.descripcion
+        binding.creatorName.text = initiatives?.creador
+        binding.votesText.text = initiatives?.totalVotantes.toString()
+    }
+}
--- app/src/main/java/com/example/bepart/main/repository/MainDataSource.kt
+++ app/src/main/java/com/example/bepart/main/repository/MainDataSource.kt
@@ -3,5 +3,10 @@ package com.example.bepart.main.repository
 import com.example.bepart.main.model.Initiatives
 
 interface MainDataSource {
+
     suspend fun getAllInitiatives(): List<Initiatives>
+
+    suspend fun addVote(initiativeKey: String, votersName: String)
+
+    suspend fun deleteVote(initiativeKey: String, votersName: String)
 }
